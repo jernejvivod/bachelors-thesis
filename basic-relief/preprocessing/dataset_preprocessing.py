c7bde668993835e9ea5ced5cf85e2aa3da0fe005
Jernej Vivod <je.vivod@gmail.com>
2019-02-19 19:46:05 +0100

    Add data preprocessing for basic relief example

--- basic-relief/preprocessing/dataset_preprocessing.py
+++ basic-relief/preprocessing/dataset_preprocessing.py
@@ -1,10 +1,17 @@
 import numpy as np
 import gzip
+import pdb
 
 
 def get_tidy_data(filename):
     with gzip.open(filename, 'rt') as csvfile:
         raw_X = np.genfromtxt(csvfile, delimiter=',', encoding='utf-8', dtype='str')
-        features = np.hstack((np.matrix(raw_X[1:, [0]]), raw_X[1:, 2:8]))
-        target = raw_X[1:, [1]]
-        return features, target
+        features = np.hstack((raw_X[1:, [0]], raw_X[1:, 2:8]))
+        target = raw_X[1:, 1]
+        for k, col in enumerate(features.T):
+            col_temp = np.array(list(map(lambda x: '0' if x == '?' else x, col)))
+            avg_val = sum(col_temp.astype(float))/len(col_temp)
+            for i, val in enumerate(features[:, k]):
+                if val == '?':
+                    features[i, k] = str(avg_val)
+        return features.astype(float), target.astype(int)
---
+++ basic-relief/relief_demo.py
@@ -0,0 +1,4 @@
+import numpy as np
+from preprocessing import dataset_preprocessing
+
+X, y = dataset_preprocessing.get_tidy_data('./preprocessing/data/auto-mpg.gz')
