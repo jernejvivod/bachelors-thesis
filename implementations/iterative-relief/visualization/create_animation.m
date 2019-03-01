97dd0e27d743e43fd8e0d6e07155130041c619b5
Jernej Vivod <je.vivod@gmail.com>
2019-03-01 10:31:46 +0100

    Finish MATLAB prototype Iterative Relief implementation

--- implementations/iterative-relief/visualization/create_animation.m
+++ implementations/iterative-relief/visualization/create_animation.m
@@ -2,7 +2,7 @@
 data1 = [0 0 0 0; 0 0 1 0; 0 1 1 1; 0 1 0 1; 1 0 0 1; 1 0 1 1; 1 1 0 0; 1 1 0 0];
 
 % Set m and k parameters.
-m = 50; min_incl = 2;
+m = 50; min_incl = 3;
 
 % Test data 2
 data2 = load('rba_test_data.m');
@@ -16,9 +16,10 @@ use_deletions = 1;
 % Set example animation timeout
 timeout = 0.01;
 
-% Create animation and display final feature weights.
-[weights, rank] = iterative_relief_animation(data3(:, 1:end-1), data3(:, end), size(data3, 1), min_incl,  @(a, b, w) minkowski_dist_weighted(a, b, w, 2), 100, 1, timeout, use_deletions);
+% Create animation and display final feature rank and weights.
+[rank, weights] = iterative_relief_animation(data3(:, 1:end-1), data3(:, end), size(data3, 1), min_incl,  @(a, b, w) minkowski_dist_weighted(a, b, w, 2), 300, 0, timeout, use_deletions);
 
+fprintf('rank = [%d, %d, %d]\n', rank);
 fprintf('weights = [%.3f, %.3f, %.3f]\n', weights);
 
 % Define weighted minkowski function that takes two vectors or matrices,
