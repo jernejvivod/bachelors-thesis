bf3d9907e75f4d48de557bc9a2808088ae85d116
Jernej Vivod <je.vivod@gmail.com>
2019-03-16 11:47:01 +0100

    Fix

--- implementations/metrics/me/itree_utils.py
+++ implementations/metrics/me/itree_utils.py
@@ -1,12 +1,11 @@
 class It_node:
     def __init__(self, l, r, split_attr, split_val, level, mass_comp=0):
-        self.l = l  # left subtree
-        self.r = r  # right subree
+        self.l = l                    # left subtree
+        self.r = r                    # right subree
         self.split_attr = split_attr  # split attribute/dimension
         self.split_val = split_val    # split value
         self.level = level            # node level
         self.mass = 0                 # node mass (number of examples in region)
-        self.mass_comp = mass_comp    # node mass computed at same time as tree is being built (compare with mass computed with traversals)
 
     # to_string: return a string encoding some information about the node
     def to_string(self):
