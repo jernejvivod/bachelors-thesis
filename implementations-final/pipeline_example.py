2ee40060cde1f4aec2b145d798fe4a123a5d951b
Jernej Vivod <je.vivod@gmail.com>
2019-05-01 09:40:17 +0200

    Change dataset

--- implementations-final/pipeline_example.py
+++ implementations-final/pipeline_example.py
@@ -18,8 +18,8 @@ import scipy.io as sio
 
 ### LOADING DATASET ######
 
-data = sio.loadmat('datasets/basic-artificial-1/data.mat')['data']
-target = np.ravel(sio.loadmat('datasets/basic-artificial-1/target.mat')['target'])
+data = sio.loadmat('datasets/ovariancancer/data.mat')['data']
+target = np.ravel(sio.loadmat('datasets/ovariancancer/target.mat')['target'])
 
 ##########################
 
@@ -50,8 +50,8 @@ euclidean_w_s.kind = 'Euclidean (weighted)'
 ### LEARNED METRIC FUNCTIONS ###
 
 # covariance/Mahalanobis distance
-covariance_dist_func = covariance.get_dist_func(data, target)
-covariance_dist_func.kind = "Mahalanobis distance"
+#covariance_dist_func = covariance.get_dist_func(data, target)
+#covariance_dist_func.kind = "Mahalanobis distance"
 
 # Mass based dissimilarity
 NUM_ITREES = 10
@@ -61,46 +61,27 @@ me_dissim_dist_func.kind = "Mass based dissimilarity"
 #################################
 
 
-
-
-### INITIAL PARAMETERS ###
-
-n_features_to_select = 2
-m = -1
-k = 5
-dist_func = lambda x1, x2 : np.sum(np.abs(x1 - x2)**2, 1)**(1.0/2.0)
-learned_metric_func = None
-max_iter = 100
-min_incl = 3
-k_width = 3
-initial_w_div = 1
-conv_condition = 1.0e-9
-
 # Initialize cross validation strategy.
 cv_startegy = StratifiedKFold(n_splits=5)
-
-##########################
-
-
 
 
 ### PIPELINES #############
 
 # Initialize pipeline for Relief algorithm
 pipeline_relief = Pipeline([('scale', StandardScaler()), 
-    ('relief', Relief(n_features_to_select=n_features_to_select, m=m, dist_func=dist_func)), 
+    ('relief', Relief()), 
     ('classify', RandomForestClassifier(n_estimators=100))])
 
 pipeline_relieff = Pipeline([('scale', StandardScaler()), 
-    ('relieff', Relieff(n_features_to_select=n_features_to_select, m=m, k=k, dist_func=dist_func)), 
+    ('relieff', Relieff()), 
     ('classify', RandomForestClassifier(n_estimators=100))])
 
 pipeline_iterative_relief = Pipeline([('scale', StandardScaler()), 
-    ('iterative_relief', IterativeRelief(n_features_to_select=n_features_to_select, m=m, min_incl=min_incl, dist_func=dist_func, max_iter=max_iter)), 
+    ('iterative_relief', IterativeRelief()), 
     ('classify', RandomForestClassifier(n_estimators=100))])
 
 pipeline_irelief = Pipeline([('scale', StandardScaler()), 
-    ('irelief', IRelief(dist_func=lambda x1, x2 : np.sum(np.abs(x1-x2)), max_iter=max_iter, k_width=k_width, conv_condition=conv_condition, initial_w_div=initial_w_div)), 
+    ('irelief', IRelief()),
     ('classify', RandomForestClassifier(n_estimators=100))])
 
 ##########################
@@ -113,26 +94,26 @@ pipeline_irelief = Pipeline([('scale', StandardScaler()),
 param_grid_relief = {
     'relief__n_features_to_select': np.arange(1, data.shape[1]+1),
     'relief__dist_func': [manhattan, euclidean],
-    'relief__learned_metric_func' : [None, covariance_dist_func]
+    'relief__learned_metric_func' : [None, me_dissim_dist_func]
 }
 
 param_grid_relieff = {
     'relieff__n_features_to_select': np.arange(1, data.shape[1]+1),
     'relieff__dist_func': [manhattan, euclidean],
-    'relieff__learned_metric_func' : [None, covariance_dist_func]
+    'relieff__learned_metric_func' : [None, me_dissim_dist_func]
 }
 
 param_grid_iterative_relief = {
     'iterative_relief__n_features_to_select': np.arange(1, data.shape[1]+1),
     'iterative_relief__dist_func' : [manhattan_w, euclidean_w],
     'iterative_relief__min_incl' : np.arange(1, 3),
-    'iterative_relief__learned_metric_func' : [None, covariance_dist_func]
+    'iterative_relief__learned_metric_func' : [None, me_dissim_dist_func]
 }
 
 param_grid_irelief = {
     'irelief__n_features_to_select': np.arange(1, data.shape[1]+1),
     'irelief__dist_func' : [manhattan_w_s, euclidean_w_s],
-    'irelief__learned_metric_func' : [None, covariance_dist_func]
+    'irelief__learned_metric_func' : [None, me_dissim_dist_func]
 }
 
 # Initialize grid search.
