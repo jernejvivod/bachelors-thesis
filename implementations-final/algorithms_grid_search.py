d9e97a2bf2250b728c067b0fe9778ad369b533b9
Jernej Vivod <je.vivod@gmail.com>
2019-05-02 11:53:42 +0200

    Fixes

--- implementations-final/algorithms_grid_search.py
+++ implementations-final/algorithms_grid_search.py
@@ -130,23 +130,23 @@ param_grid_relief = {
 param_grid_relieff = {
     'relieff__n_features_to_select': np.arange(1, data.shape[1]+1),
     'relieff__dist_func': [manhattan, euclidean],
-    'relieff__learned_metric_func' : [None, me_dissim_dist_func],#, nca_dist_func, lda_dist_func, pca_dist_func],
-    #'classify__kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
+    'relieff__learned_metric_func' : [None, me_dissim_dist_func, nca_dist_func, lda_dist_func, pca_dist_func],
+    'classify__kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
 }
 
 param_grid_iterative_relief = {
-#    'iterative_relief__n_features_to_select': np.arange(1, data.shape[1]+1),
+    'iterative_relief__n_features_to_select': np.arange(1, data.shape[1]+1),
     'iterative_relief__dist_func' : [manhattan_w, euclidean_w],
     'iterative_relief__min_incl' : np.arange(3, 6),
-    'iterative_relief__learned_metric_func' : [None, me_dissim_dist_func],#, nca_dist_func, lda_dist_func, pca_dist_func],
-#    'classify__kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
+    'iterative_relief__learned_metric_func' : [None, me_dissim_dist_func, nca_dist_func, lda_dist_func, pca_dist_func],
+    'classify__kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
 }
 
 param_grid_irelief = {
     'irelief__n_features_to_select': np.arange(1, data.shape[1]+1),
     'irelief__dist_func' : [manhattan_w_s, euclidean_w_s],
     'irelief__learned_metric_func' : [None, me_dissim_dist_func, nca_dist_func, lda_dist_func, pca_dist_func],
-    #'classify__kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
+    'classify__kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
 }
 
 param_grid_multiSURF = {
@@ -171,8 +171,8 @@ grid_search_irelief = GridSearchCV(pipeline_irelief, param_grid=param_grid_ireli
 grid_search_random_selection = GridSearchCV(pipeline_random_selection, param_grid=param_grid_random_selection, cv=cv_startegy, verbose=True, n_jobs=-1)
 
 # Perform grid search for best hyperparameters.
-# res_relief = grid_search_relief.fit(data, target)
-# res_relieff = grid_search_relieff.fit(data, target)
+res_relief = grid_search_relief.fit(data, target)
+res_relieff = grid_search_relieff.fit(data, target)
 # res_iterative_relief = grid_search_iterative_relief.fit(data, target)
 res_irelief = grid_search_irelief.fit(data, target)
 # res_multiSURF = grid_search_multiSURF.fit(data, target)
